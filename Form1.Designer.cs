fcc8dd5f58b595df7b67c79d112b93dc1d7a3d80
iremcan <irem.1206.can@gmail.com>
2026-07-28 23:58:19 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace odev.rehber
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.txtsoyad = new System.Windows.Forms.TextBox();
+            this.txttelefon = new System.Windows.Forms.TextBox();
+            this.txteposta = new System.Windows.Forms.TextBox();
+            this.btnekle = new System.Windows.Forms.Button();
+            this.btnguncelle = new System.Windows.Forms.Button();
+            this.btnsil = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ad";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 261);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 302);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "telefon";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(77, 342);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "eposta";
+            // 
+            // txtad
+            // 
+            this.txtad.Location = new System.Drawing.Point(210, 221);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(100, 22);
+            this.txtad.TabIndex = 5;
+            // 
+            // txtsoyad
+            // 
+            this.txtsoyad.Location = new System.Drawing.Point(210, 255);
+            this.txtsoyad.Name = "txtsoyad";
+            this.txtsoyad.Size = new System.Drawing.Size(100, 22);
+            this.txtsoyad.TabIndex = 6;
+            // 
+            // txttelefon
+            // 
+            this.txttelefon.Location = new System.Drawing.Point(210, 302);
+            this.txttelefon.Name = "txttelefon";
+            this.txttelefon.Size = new System.Drawing.Size(100, 22);
+            this.txttelefon.TabIndex = 7;
+            // 
+            // txteposta
+            // 
+            this.txteposta.Location = new System.Drawing.Point(210, 342);
+            this.txteposta.Name = "txteposta";
+            this.txteposta.Size = new System.Drawing.Size(100, 22);
+            this.txteposta.TabIndex = 8;
+            // 
+            // btnekle
+            // 
+            this.btnekle.Location = new System.Drawing.Point(421, 258);
+            this.btnekle.Name = "btnekle";
+            this.btnekle.Size = new System.Drawing.Size(75, 23);
+            this.btnekle.TabIndex = 9;
+            this.btnekle.Text = "ekle";
+            this.btnekle.UseVisualStyleBackColor = true;
+            this.btnekle.Click += new System.EventHandler(this.btnekle_Click);
+            // 
+            // btnguncelle
+            // 
+            this.btnguncelle.Location = new System.Drawing.Point(540, 258);
+            this.btnguncelle.Name = "btnguncelle";
+            this.btnguncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnguncelle.TabIndex = 10;
+            this.btnguncelle.Text = "güncelle";
+            this.btnguncelle.UseVisualStyleBackColor = true;
+            this.btnguncelle.Click += new System.EventHandler(this.btnguncelle_Click);
+            // 
+            // btnsil
+            // 
+            this.btnsil.Location = new System.Drawing.Point(478, 317);
+            this.btnsil.Name = "btnsil";
+            this.btnsil.Size = new System.Drawing.Size(75, 23);
+            this.btnsil.TabIndex = 11;
+            this.btnsil.Text = "sil";
+            this.btnsil.UseVisualStyleBackColor = true;
+            this.btnsil.Click += new System.EventHandler(this.btnsil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnsil);
+            this.Controls.Add(this.btnguncelle);
+            this.Controls.Add(this.btnekle);
+            this.Controls.Add(this.txteposta);
+            this.Controls.Add(this.txttelefon);
+            this.Controls.Add(this.txtsoyad);
+            this.Controls.Add(this.txtad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.TextBox txtsoyad;
+        private System.Windows.Forms.TextBox txttelefon;
+        private System.Windows.Forms.TextBox txteposta;
+        private System.Windows.Forms.Button btnekle;
+        private System.Windows.Forms.Button btnguncelle;
+        private System.Windows.Forms.Button btnsil;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace odev.rehber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists("rehber.xml"))
+            {
+                XmlTextWriter dosya = new XmlTextWriter("rehber.xml", Encoding.UTF8);
+                dosya.Formatting = Formatting.Indented;
+                dosya.WriteStartDocument();
+                dosya.WriteStartElement("Kisiler");
+                dosya.WriteEndElement();
+                dosya.Close();
+            }
+
+
+            KayitlariGetir();
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+                txtad.Text = row.Cells["Ad"]?.Value?.ToString() ?? "";
+                txtsoyad.Text = row.Cells["soyad"]?.Value?.ToString() ?? "";
+                txttelefon.Text = row.Cells["telefon"]?.Value?.ToString() ?? "";
+                txteposta.Text = row.Cells["eposta"]?.Value?.ToString() ?? "";
+
+            }
+        }
+        private void KayitlariGetir()
+        {
+            if (File.Exists("rehber.xml"))
+            {
+                DataSet dset = new DataSet();
+                XmlReader reader = XmlReader.Create("rehber.xml");
+                dset.ReadXml(reader);
+                reader.Close();
+
+                if (dset.Tables.Count > 0)
+                {
+                    dataGridView1.DataSource = dset.Tables[0];
+
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        if (row.Cells["eposta"].Value != null)
+                        {
+                            string eposta = row.Cells["eposta"].Value.ToString();
+                            string adSoyad = row.Cells["Ad"].Value?.ToString() + " " + row.Cells["soyad"].Value?.ToString();
+                            row.Cells["eposta"].Value = eposta;
+                        }
+                    }
+                }
+                else
+                {
+                }
+            }
+        }
+
+        private void btnekle_Click(object sender, EventArgs e)
+        {
+            string ad = txtad.Text.Trim();
+            string soyad = txtsoyad.Text.Trim();
+            string telefon = txttelefon.Text.Trim();
+            string eposta = txteposta.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(ad))
+            {
+                MessageBox.Show("Ad  boş olamaz."); return;
+            }
+
+            if (!Regex.IsMatch(telefon, @"^05\d{9}$"))
+
+            {
+                MessageBox.Show("Telefon hatalı. 05xxxxxxxxx formatında giriniz."); return;
+            }
+            if (!Regex.IsMatch(eposta, @"^[\w\.-]+@[\w\.-]+\.\w+$"))
+            {
+                MessageBox.Show("Geçerli bir e-posta adresi giriniz.");
+                return;
+            }
+
+
+            XDocument xdoc = XDocument.Load("rehber.xml");
+            bool kisiVarMi = xdoc.Root.Elements("Kisi")
+        .Any(k =>
+            (string)k.Element("Ad") == ad &&
+            (string)k.Element("soyad") == soyad
+        );
+
+            if (kisiVarMi)
+            {
+                MessageBox.Show("Bu isimle bir kişi zaten var!");
+                return;
+            }
+            XElement yeniKisi = new XElement("Kisi",
+                new XElement("Ad", ad),
+                new XElement("soyad", soyad),
+                new XElement("telefon", telefon),
+                new XElement("eposta", eposta)
+            );
+            xdoc.Root.Add(yeniKisi);
+            xdoc.Save("rehber.xml");
+
+            MessageBox.Show("Kayıt eklendi.");
+
+            txteposta.Clear();
+
+            KayitlariGetir();
+            txtad.Clear();
+            txtsoyad.Clear();
+            txttelefon.Clear();
+            txteposta.Clear();
+
+
+            bool kisiVarni = xdoc.Root.Elements("Kisi")
+                .Any(k =>
+                    (string)k.Element("Ad") == txtad.Text.Trim() &&
+                    (string)k.Element("soyad") == txtsoyad.Text.Trim()
+                );
+
+            if (kisiVarMi)
+            {
+                MessageBox.Show("Bu isimle bir kişi zaten var!");
+                return;
+            }
+        }
+
+        private void btnsil_Click(object sender, EventArgs e)
+        {
+            XDocument xdoc = XDocument.Load("rehber.xml");
+
+            string ad = txtad.Text.Trim().ToLower();
+            XElement kisi = xdoc.Root.Elements("Kisi")
+                .FirstOrDefault(k => ((string)k.Element("Ad"))?.ToLower() == ad);
+
+
+            if (kisi != null)
+            {
+                kisi.Remove();
+                xdoc.Save("rehber.xml");
+                MessageBox.Show("Kayıt silindi.");
+                KayitlariGetir();
+            }
+            else
+            {
+                MessageBox.Show("Silinecek kişi bulunamadı.");
+            }
+        }
+
+        private void btnguncelle_Click(object sender, EventArgs e)
+        {
+            XDocument xdoc = XDocument.Load("rehber.xml");
+
+            XElement kisi = xdoc.Root.Elements("Kisi")
+                .FirstOrDefault(k => (string)k.Element("Ad") == txtad.Text);
+
+            if (kisi != null)
+            {
+                kisi.Remove();
+                XElement yeniKisi = new XElement("Kisi",
+                    new XElement("Ad", txtad.Text),
+                    new XElement("soyad", txtsoyad.Text),
+                    new XElement("telefon", txttelefon.Text),
+                    new XElement("eposta", txteposta.Text)
+                );
+
+                xdoc.Root.Add(yeniKisi);
+                xdoc.Save("rehber.xml");
+                KayitlariGetir();
+
+                MessageBox.Show("Kayıt güncellendi.");
+            }
+            else
+            {
+                MessageBox.Show("Güncellenecek kişi bulunamadı.");
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
